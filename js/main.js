6ce119d89a68a141cf682baba1f633ae714ce232
daryagah <59887251+daryagah@users.noreply.github.com>
2020-03-15 19:21:24 -0400

    final

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-/**
- * Your JS code here
- */
+document.addEventListener('DOMContentLoaded', function () {
+    var ulElement = document.getElementById('links');
+    var liElement = document.createElement('li');
+    liElement.appendChild(document.createTextNode('created by main.js'));
 
-/**
- * Sample code
- */
-document.addEventListener('DOMContentLoaded', function() {
-  var ulElement = document.getElementById('links');
-  var liElement = document.createElement('li');
-  liElement.appendChild(document.createTextNode('created by main.js'));
-
-  ulElement.appendChild(liElement);
+    ulElement.appendChild(liElement);
 });
+
+
+(function($) {
+
+		$('.scrolly').scrolly();
+
+})(jQuery);
